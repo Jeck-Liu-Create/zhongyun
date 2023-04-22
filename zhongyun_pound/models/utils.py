d78092d935d8586e6598ee493677bf53531dedfb
[Jeck-Liu-Create] <[tclyldx@163.com]>
2023-04-22 16:01:12 +0800

    磅单模块增加LocalToUtcPlus函数，将<=UTC时间新增一天

--- zhongyun_pound/models/utils.py
+++ zhongyun_pound/models/utils.py
@@ -1,5 +1,6 @@
 import pytz
 import datetime
+
 """ 实现utc时间转本地时间 """
 
 
@@ -19,8 +20,16 @@ def LocalToUtc(local_date):
     # 将本地日期时间转换为UTC日期时间
     utc_datetime = local_datetime.astimezone(pytz.utc)
 
-    # 仅获取UTC日期部分
-    utc_date = utc_datetime.date()
+    return utc_datetime
 
-    return utc_date
 
+def LocalToUtcPlus(local_date):
+    local_tz = pytz.timezone('Asia/Shanghai')
+
+    # 将本地日期转换为本地日期时间
+    local_datetime = local_tz.localize(datetime.datetime.combine(local_date, datetime.time.min))
+
+    # 将本地日期时间转换为UTC日期时间 并增加一天
+    utc_datetime = local_datetime.astimezone(pytz.utc) + datetime.timedelta(days=1)
+
+    return utc_datetime
